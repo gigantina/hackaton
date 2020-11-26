959f5d3c69003098161a88a1d62b4d4d26caf98a
1100319813 <nstareeva@gmail.com>
2020-11-26 07:39:58 -0500

    change_password

--- Volonter/Volonter/urls.py
+++ Volonter/Volonter/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from TestApp import views
+from django.conf.urls import url
+from django.contrib.auth.views import login, logout, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 
 urlpatterns = [
     path('', views.main_page),
@@ -31,4 +33,8 @@ urlpatterns = [
 	url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
 	path('/comment/', AddCommentView.as_view(), name='add_comment'),
 	url(r'^change-password/$', views.change_password, name='change_password'),
+	url(r'^reset_password/$', password_reset, name='reset_password'),
+	url(r'^reset_password/done/$', password_reset_done, name='reset_password_done'),
+	url(r'^reset_password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?)$P<token>+/$', password_reset_confirm, name='reset_password_confirm'),
+	url(r'^reset_password/complete/$', password_reset_complete, name='reset_password_complete'),
 ]
